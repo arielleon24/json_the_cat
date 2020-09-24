1c73c06d825cce751af59dcb4b299b9d36865012
Ariel Leon <arielleon@MacBook-Pro.local>
2020-09-24 16:38:11 -0400

    breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,22 @@
 const request = require("request");
-const rq = request; 
 
-let nodeInput = process.argv[2];
-let apiURL = `https://api.thecatapi.com/v1/breeds/search?q=${nodeInput}`
 
-rq(apiURL, (error, response, body) => {
-  if (error) {  
-    console.log("Request Failed:", error);
-    return
-  } 
-  const objBody = JSON.parse(body)  
-  if (objBody.length === 0 ) {
-    console.log("Breed not found:", nodeInput)
-  } else {
-    console.log(objBody[0].description)
-  }
-})
+
+const fetchBreedDescription = function(breedName, callback) {
+  let apiURL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
+  request(apiURL, (error, response, body) => {
+    if (error) {  
+      callback(error)
+      return
+    } else {
+      const objBody = JSON.parse(body) 
+      if (objBody.length === 0 ) {
+      console.log("Breed not found:", breedName)
+    } else {
+      console.log(objBody[0].description)
+    }
+   }
+  })
+};
+
+module.exports = { fetchBreedDescription }
